978fc1a593f17d656d9a16927d66f28f39ac890d
Lucas Honda <eliashondalucas@hotmail.com>
2023-06-06 14:11:53 -0300

    image by user

---
+++ meau/utils/userFromStorage.js
@@ -0,0 +1,15 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const userFromStorage = async () => {
+    try {
+      const user = await AsyncStorage.getItem('user');
+      if (user !== null) {
+        return JSON.parse(user);
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.log('Erro ao recuperar o usuário da storage:', error);
+      return null;
+    }
+  };
