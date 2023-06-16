d9c72f53a3dc8497ac2f0d802856ffc2e92b2179
Pericles12 <pericles@gbtsa.com.br>
2023-06-16 18:38:26 -0300

    Tela PetsAdotar
    TelaAdotar Pet

--- meau/Rotas/index.js
+++ meau/Rotas/index.js
@@ -7,6 +7,9 @@ import RegistrarUsuario from "../Telas/CadastroPessoal_Registro";
 import "react-native-gesture-handler";
 import MeusPets1 from "../Telas/MeusPets";
 import DadosPet from "../Telas/DadosPet";
+import DadosPetsAdotar from "../Telas/DadosPetsAdotar";
+import PetsAdotar from "../Telas/PetsAdotar";
+
 
 const Drawer = createDrawerNavigator();
 
@@ -48,7 +51,11 @@ export default function Rotas() {
         />
         <Drawer.Screen name="Cadastrar Pet" component={CadastrarPet} />
         <Drawer.Screen name="Meus Pets" component={MeusPets1} />
+        <Drawer.Screen name="Pets Adotar" component={PetsAdotar} />
         <Drawer.Screen name="Dados Pets" component={DadosPet} />
+        <Drawer.Screen name="Dados Pets Adotar" component={DadosPetsAdotar} />
+        
+        
       </Drawer.Navigator>
     );
   } else {
@@ -63,3 +70,4 @@ export default function Rotas() {
     );
   }
 }
+
